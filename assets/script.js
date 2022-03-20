dfe5924a620c93f3a9472248a8492774572ff6ce
Sam Knopper <samanthaknopper@hotmail.com>
2022-03-20 19:31:49 +0800

    framework of index changed, javascript simplified, colours working, save working, retrieving not working

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 const timeContainter = $('#time-containter');
 
+//getNote();
+
 // Show current time and date in jumbo
 function displayTime() {
 var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -9,82 +11,42 @@ console.log("#currentDay");
 }
 setInterval(displayTime, 1000);
 
-function createRow(time){
-    const timestamp =time + ':00';
 
-    const row = $("<div>").attr('class', 'row time-block border');
-
-    const timeCol = $("<div>").attr('class', 'hour row col-2').text(timestamp);
-
-    row.append(timeCol);
-
-    const inputCol = $("<div>").attr('class', 'col-8');
-
-    
-
-
-    const input = $("<input>").attr('type', 'text').attr('class','input-note textarea');
-     
-    inputCol.append(input);
-    //check if there is existing data
-    const existingNote = getNote(time);
-    if(existingNote){
-        input.val(existingNote);
-    }
-    row.append(inputCol);
-
-    const buttonCol = $("<div>").attr('class', 'col-2' );
-
-    const button = $("<button>").attr('class', 'saveBtn ').text('Save');
-
-    buttonCol.append(button);
-
-    row.append(buttonCol);
-
-    return row;
-}
-//create time blocks
-for (let time = 9; time < 18; time++) {
-    
-    const row = createRow(time);
-    timeContainter.append(row);
-}
-
-function getNote(time) {
-    const timestamp = time + ':00';
-
-    return localStorage.getItem(timestamp)
-}
 
 function saveNote(note, time) {
     localStorage.setItem(time, note);
 
 }
+// function getNote(time) {
+//     const timestamp = time + ':00';
 
+//     return localStorage.getItem(timestamp);
+// }
+
+// const existingNote = getNote(time);
+
+// if(existingNote){
+//     input.val(existingNote);
+// }
 //user inputs text into time block 
-$('.input-note').on('change', document, function(event){
-
-    const inputEl = $(event.target);
-
-    const timestamp = inputEl.parent().prev().text();
-
-    const userInput = inputEl.val();
-
-    saveNote(userInput, timestamp);
-
-})
 
 $('.saveBtn').on("click", document, function(event){
-    const inputEl = $(event.target).parent().prev().children()[0]
+    const inputEl = $(event.target).siblings('.textarea');
+    console.log(inputEl);
 
     const userInput = $(inputEl).val();
 
-    const timeEl = $(event.target).parent().prev().prev();
+    const timeEl = $(event.target).prev().prev();
 
     const timestamp = timeEl.text();
 
-    saveNote(userInput, timestamp)
+    saveNote(userInput, timestamp);
 });
+
+
+
+
+
 
 //colour changes with time
 var checkTime = function () {
@@ -92,11 +54,11 @@ var checkTime = function () {
     //Get Current time
     var currentTime = moment().format('H');
 
-    //get all elements with class "taskarea"
+    //get all elements with class "textarea"
     var timeBlockElements = $(".textarea");
     
 
-    //loop through textarea classes
+    //loop through taskarea classes
     for (var i = 0 ; i < timeBlockElements.length ; i++) {
 
         //Get element i's ID as a string
