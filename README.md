beb375c53554715de85cce531678e5ce5361c635
Sam Knopper <samanthaknopper@hotmail.com>
2022-03-21 22:47:04 +0800

    reverted back to dynamically created tables, colour working, readME completed, minor css issues

--- README.md
+++ README.md
@@ -4,11 +4,7 @@ Interactice calendar to book out time in a work day.
 This calendar allows the user to plan their day in hourly blocks. The time that has past turns grey, current time is green and time in the future is red. User types in the middle box to set reminders or provide information about the task, then they click the save button. This saves the text to their local storage. A reset button was added for improved user functionality, in case they make a mistake and want to restart the day. 
 
 ## Screenshots
-<img src="assets\Screenshot1.jpg" >
-<img src="assets\Screenshot2.jpg">
-<img src="assets\Screenshot3.jpg">
-<img src="assets\Screenshot4.jpg">
-<img src="assets\Screenshot5.jpg">
+<img src="assets\images\Screenshot1.jpg" >
 
 ## References
 w3schools https://www.w3schools.com
--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,119 @@
 const timeContainter = $('#time-containter');
-const clearBtn = $('.clearBtn');
 
-$('.clearBtn').on("click", document, function(event){
+const clearBtn = $('.clearBtn');
+// button to reset local storage
+$('.clearBtn').on("click", document, function (event) {
     localStorage.clear();
     window.location.reload();
 })
 
-//getNote();
 
 // Show current time and date in jumbo
 function displayTime() {
-var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-$("#currentDay").text(now);
-console.log("#currentDay");
+    var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    $("#currentDay").text(now);
+    console.log("#currentDay");
 
 }
 setInterval(displayTime, 1000);
 
+function createRow(time) {
+    var objDate = new Date();
+    var currentHour = objDate.getHours();
+    
+    console.log(currentHour);
+    console.log(time);
+ 
+    
 
+
+    const timestamp = time + ':00';
+
+    const row = $("<div>").attr('class', 'row time-block border').attr('id',`${time}-row`);
+
+    //check time vs current hour
+
+    if (time === currentHour) {
+        $(row).addClass('present')
+    }
+    else if (time < currentHour) {
+        $(row).addClass('past')
+    }
+    else { 
+        $(row).addClass('future')
+    }
+  
+
+    const timeCol = $("<div>").attr('class', 'hour row col-1 timesection').text(timestamp);
+
+    row.append(timeCol);
+
+    const inputCol = $("<div>").attr('class', 'col-10');
+
+    const input = $("<textarea>").attr('type', 'text').attr('class', 'input-note textarea');
+
+    inputCol.append(input);
+    //check if there is existing data
+    const existingNote = getNote(time);
+    if (existingNote) {
+        input.val(existingNote);
+    }
+    row.append(inputCol);
+
+    const buttonCol = $("<div>").attr('class', 'col-1');
+
+    const button = $("<button>").attr('class', 'saveBtn ').text('Save');
+
+    buttonCol.append(button);
+
+    row.append(buttonCol);
+
+    return row;
+
+
+}
+//create time blocks
+for (let time = 9; time < 18; time++) {
+
+    const row = createRow(time);
+    timeContainter.append(row);
+}
+
+function getNote(time) {
+    const timestamp = time + ':00';
+
+    return localStorage.getItem(timestamp)
+}
 
 function saveNote(note, time) {
     localStorage.setItem(time, note);
 
 }
-// function getNote(time) {
-//     const timestamp = time + ':00';
 
-//     return localStorage.getItem(timestamp);
-// }
-
-// const existingNote = getNote(time);
-
-// if(existingNote){
-//     input.val(existingNote);
-// }
 //user inputs text into time block 
+$('.input-note').on('change', document, function (event) {
 
-$('.saveBtn').on("click", document, function(event){
-    const inputEl = $(event.target).siblings('.textarea');
-    console.log(inputEl);
+    const inputEl = $(event.target);
+
+    const timestamp = inputEl.parent().prev().text();
+
+    const userInput = inputEl.val();
+
+    saveNote(userInput, timestamp);
+
+})
+
+$('.saveBtn').on("click", document, function (event) {
+    const inputEl = $(event.target).parent().prev().children()[0]
 
     const userInput = $(inputEl).val();
 
-    const timeEl = $(event.target).prev().prev();
+    const timeEl = $(event.target).parent().prev().prev();
 
     const timestamp = timeEl.text();
 
-    saveNote(userInput, timestamp);
+    saveNote(userInput, timestamp)
 });
 
-
-
-
-
-
-//colour changes with time
-var checkTime = function () {
-
-    //Get Current time
-    var currentTime = moment().format('H');
-
-    //get all elements with class "textarea"
-    var timeBlockElements = $(".textarea");
-    
-
-    //loop through taskarea classes
-    for (var i = 0 ; i < timeBlockElements.length ; i++) {
-
-        //Get element i's ID as a string
-        var elementID = timeBlockElements[i].id;
-
-        //get element by ID
-        var manipID = document.getElementById(timeBlockElements[i].id)
-        
-        //remove any old classes from element
-        $(timeBlockElements[i].id).removeClass(".present .past .future");
-
-        // apply new class if task is present/past/future
-        if (elementID < currentTime) {
-            $(manipID).addClass("past");
-        } else if (elementID > currentTime) {
-            $(manipID).addClass("future");
-        } else {
-            $(manipID).addClass("present");
-        }
-    }
-}
-
-// checkTime every 5 minutes
-setInterval(checkTime(), (1000 * 60) * 5);
+// checkbackGround every 5 minutes
+setInterval(setBackgroundColor(), (1000 * 60) * 5);
